85f745669e6ea970986d08bb344ddcf7b35c9d5a
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-01-03 00:07:38 -0300

    search functions

--- t01/ordenacao.c
+++ t01/ordenacao.c
@@ -12,25 +12,41 @@ unsigned int getGRR() {
 	return 20220072;
 }
 
+int aux_buscaSequencial(int vetor[], int tam, int valor, int *numComparacoes) {
+	(*numComparacoes)++;
+	if (valor == vetor[tam-1]) return tam-1;
+
+	return aux_buscaSequencial(vetor, tam-1, valor, numComparacoes);
+}
+
 int buscaSequencial(int vetor[], int tam, int valor, int *numComparacoes) {
 	if (tam < 1) return -1;
 
-	*numComparacoes++;
-	if (valor = vetor[tam-1]) return tam-1;
+	(*numComparacoes)++;
+	if(valor > vetor[tam-1]) return -1;
+	
+	(*numComparacoes)++;
+	if (valor < vetor[0]) return -1;
 
-	return buscaSequencial(vetor, tam-1, valor, numComparacoes);
+	return aux_buscaSequencial(vetor, tam, valor, numComparacoes);
+}
+
+int aux_buscaBinaria(int vetor[], int inicio, int fim, int valor, int *numComparacoes) {
+	if (fim - inicio < 0) return -1;
+
+	int m = (fim + inicio) / 2;
+
+	(*numComparacoes)++;
+	if (valor == vetor[m]) return m;
+
+	(*numComparacoes)++;
+	if (valor < vetor[m]) return aux_buscaBinaria(vetor, inicio, m-1, valor, numComparacoes);
+
+	return aux_buscaBinaria(vetor, m+1, fim, valor, numComparacoes);
 }
 
 int buscaBinaria(int vetor[], int tam, int valor, int *numComparacoes) {
-	if (tam < 1) return -1;
-	
-	int m = (tam + 1) / 2;
-	*numComparacoes++;
-	if (valor < vetor[m]) return buscaBinaria(vetor, m-1, valor, numComparacoes);
-	
-	//TODO: não implementado
-	*numComparacoes++;
-	if (valor > vetor[m]) return buscaBinaria(, m-1, valor, numComparacoes);
+	return aux_buscaBinaria(vetor, 0, tam-1, valor, numComparacoes);
 }
 
 int insertionSort(int vetor[], int tam) {
