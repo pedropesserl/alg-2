31b14a0f0e789938612e78d88a9603db9d0b078b
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-02-14 00:21:23 -0300

    comments

--- t02/main.c
+++ t02/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEM_ERROR_EXIT                                    \
-	do {                                                  \
-		fprintf(stderr, "Erro na alocação de memória.");  \
-		exit(1);                                          \
-	} while(0)
-
 typedef struct {
 	double valor;
 	double peso;
@@ -23,14 +17,14 @@ void imprime_vetor(int vetor[], int tam) {
 	printf("%d\n", vetor[tam-1]);
 }
 
-void acha_solucao(int vbin[], presente_t presentes[], int tam, double *valor_maximo, int indices_solucao[], int *tam_solucao) {
+void trata_solucao(int vbin[], presente_t presentes[], int tam, double *valor_maximo, int indices_solucao[], int *tam_solucao) {
 	double valor_total = 0;
-	int j = 0;
 
 	for (int i = 0; i < tam; i++)
 		if (vbin[i])
 			valor_total += presentes[i].valor;
 
+	int j = 0;
 	if (valor_total > *valor_maximo) {
 		for (int i = 0; i < tam; i++)
 			if (vbin[i]) {
@@ -53,17 +47,24 @@ int vetor_valido(int vbin[], presente_t presentes[], int tam, int p_max) {
 	return peso_presentes <= p_max;
 }
 
-void vetores_binarios(int vbin[], presente_t presentes[], int tam, int p_max, int i, double *valor_maximo, int indices_solucao[], int *tam_solucao) {
-	if (i == tam) {
+void acha_solucoes(int vbin[], presente_t presentes[], int tam, int p_max, double *valor_maximo, int indices_solucao[], int *tam_solucao, int i) {
+	// essa função implementa o backtracking do algoritmo criando recursivamente todos os
+	// possíveis vetores binários de tamanho tam
+
+	if (i == tam) { // caso base
+		// checa se o subconjunto do conjunto de presentes gerado pelo
+		// vetor binário tem peso válido
 		if (vetor_valido(vbin, presentes, tam, p_max))
-			acha_solucao(vbin, presentes, tam, valor_maximo, indices_solucao, tam_solucao);
+			// escreve o vetor de indices dos presentes encontrados e
+			// define o valor sentimental máximo encotrado até o momento
+			trata_solucao(vbin, presentes, tam, valor_maximo, indices_solucao, tam_solucao);
 		return;
 	}
 
 	vbin[i] = 0;
-	vetores_binarios(vbin, presentes, tam, p_max, i+1, valor_maximo, indices_solucao, tam_solucao);
+	acha_solucoes(vbin, presentes, tam, p_max, valor_maximo, indices_solucao, tam_solucao, i+1);
 	vbin[i] = 1;
-	vetores_binarios(vbin, presentes, tam, p_max, i+1, valor_maximo, indices_solucao, tam_solucao);
+	acha_solucoes(vbin, presentes, tam, p_max, valor_maximo, indices_solucao, tam_solucao, i+1);
 }
 
 void resolve_papai_noel(int tam, int p_max, presente_t presentes[]) {
@@ -72,7 +73,7 @@ void resolve_papai_noel(int tam, int p_max, presente_t presentes[]) {
 	int indices_solucao[tam];
 	int vbin[tam];
 	
-	vetores_binarios(vbin, presentes, tam, p_max, 0, &valor_solucao, indices_solucao, &tam_solucao);
+	acha_solucoes(vbin, presentes, tam, p_max, &valor_solucao, indices_solucao, &tam_solucao, 0);
 
 	imprime_vetor(indices_solucao, tam_solucao);
 	printf("%lf\n", valor_solucao);
